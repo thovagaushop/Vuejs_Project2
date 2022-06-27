5859d2220ea57a3026bcf29c2408fcdd51c4026a
thovagaushop <minhmeomeomun@gmail.com>
2022-06-27 17:09:53 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+import {messages} from './json.js';
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      messages,
+      message : messages[Math.floor(Math.random() * messages.length)]
+    }
+  },
+  methods : {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/json.js
@@ -0,0 +1,6 @@
+export const messages = [
+    "Happy new year 2022",
+    "'Happy Valentine day'",
+    "'Have a nice day'",
+    "'You have receive a code for paymant'",
+]
